18cd3d4f2a6ac48b2f853721857a674d8847bf8d
MAZE1XA <90650931+MAZE1XA@users.noreply.github.com>
2022-03-11 15:09:54 -0700

    started the assignment

---
+++ app.js
@@ -0,0 +1,14 @@
+  // set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector(#value');
+ const btns = documet.querySelectorAll(".btn") 
+
+ btns.forEach(function (item)  {
+ consol.log    
+     
+ });
+
+
+
