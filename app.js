a11a4c11118872320df4227f0f9cc6fce56d1d9d
MAZE1XA <90650931+MAZE1XA@users.noreply.github.com>
2022-03-16 15:54:43 -0600

    added more coding

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ const value = document.querySelector(#value');
  const btns = documet.querySelectorAll(".btn") 
 
  btns.forEach(function (item)  {
- consol.log    
-     
+ btn.addEventListener("click",function (e) { 
+  const styles = e.currentTarget.classList   
+ if(styles.containes('decrease')){
+  count--;
+ }else if(styles.containes('increase'))
+count++;
+ }else{ 
+  count=0;
+ }
+ if(count>0){                    
+ value.style.color="green";
+ }
+  if(count<0)
+   value.style.color='red'   
+ }
+ if(count===0){
+value.style.color="#222";
+     value.textContent=count;
  });
-
-
-
+});
